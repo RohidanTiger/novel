6b90f17f66b0bd9ee414a7d9591c9b33030a33cd
quydv <ongbutdeptrai9x@gmail.com>
2021-03-30 22:25:52 +0700

    first commit

---
+++ feature_explore/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id(GradlePluginId.ANDROID_DYNAMIC_FEATURE)
+    id(GradlePluginId.KOTLIN_ANDROID)
+    id(GradlePluginId.KOTLIN_ANDROID_EXTENSIONS)
+    id(GradlePluginId.SAFE_ARGS)
+}
+
+android {
+    compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
+
+    defaultConfig {
+        minSdkVersion(AndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(AndroidConfig.TARGET_SDK_VERSION)
+
+        versionCode = AndroidConfig.VERSION_CODE
+        versionName = AndroidConfig.VERSION_NAME
+        testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        getByName(BuildType.RELEASE) {
+            isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+            proguardFiles("proguard-android.txt", "proguard-rules.pro")
+        }
+
+        getByName(BuildType.DEBUG) {
+            isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    sourceSets {
+        getByName("test").java.srcDir("${project.rootDir}/app/build/generated/source/navigation-args/debug")
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = TestOptions.IS_RETURN_DEFAULT_VALUES
+    }
+}
+
+dependencies {
+    implementation(project(ModuleDependency.APP))
+    androidTestImplementation(project(":feature_explore"))
+    addTestDependencies()
+}
